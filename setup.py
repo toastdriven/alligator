256e9fa88532246d3716f51b9528c65e652a5e92
Daniel Lindsley <daniel@toastdriven.com>
2015-01-11 18:29:36 -0600

    Bumped to 0.9.0!

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='alligator',
-    version='0.9.0-dev',
+    version='0.9.0',
     description='Simple offline task queues.',
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
